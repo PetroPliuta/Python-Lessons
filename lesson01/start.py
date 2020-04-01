852322e3e3144b9db36e6759d6749c14bcef27d1
Peter Pliuta <peter@splynx.com.ua>
2020-04-01 20:33:44 +0300

    classwork

--- lesson01/start.py
+++ lesson01/start.py
@@ -44,27 +44,23 @@ import random
 #     print('a=b')
 
 # 1
-# a = int(input('Enter km:'))
+# a = int(input('Enter km: '))
 # print(a, "km = ", a*1000, "m")
 
 # # 2
 # USD = 25.5
 # EUR = 30.0
 # RUB = .3
-
 # s = float(input("Enter sum (UAH): "))
 # print("EUR = ", s/EUR)
 # print("USD = ", s/USD)
 # print("RUB = ", s/RUB)
 
 # 3
-
 # price = float(input("Enter price: "))
 # fuel_consumption = float(input("Enter fuel consumption: "))
 # length = float(input("Enter path length: "))
-
 # res = length/100 * fuel_consumption * price
-
 # print('Result = ', res)
 
 # 4
@@ -75,11 +71,25 @@ import random
 # if a == b and b == c:
 #     print('All equal')
 
-exit = False
+# # 5
+# exit = False
+# while not exit:
+#     choice = int(input("1. Add\n2. Div\n0. Exit\n==>"))
+#     if choice == 1:
+#         print("a+b")
+#         print("sum: ", float(input("Enter a: "))+float(input("Enter b: ")))
+#     elif choice == 2:
+#         print("a/b")
+#         print("div: ", float(input("Enter a: "))/float(input("Enter b: ")))
+#     elif choice == 0:
+#         exit = True
 
-while not exit:
-    choice = int(input("1. Add\n2. Div\n0. Exit\n==>"))
-    if choice == 1:
-        print("a+b")
-    elif choice == 0:
-        exit = True
+# 6
+# -10 + 30
+counter = 0
+for number in range(7):
+    t = random.randrange(-10, 30)
+    print("T: ", t)
+    if t > 10:
+        counter += 1
+print("Count = ", counter)
