f122587f294e2e3ee51a5a64ee8bfa54b8e30341
Peter Pliuta <peter@splynx.com.ua>
2020-04-08 20:32:32 +0300

    hw3

---
+++ lesson03/classwork.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3.8
+# print("Env")
+
+from lib.calc import summ, minus, mul, div
+# import lib
+
+print("Summ: ", summ(1, 3, 4))
+print("Minus: ", minus(1, 3, 4))
+print("Mul: ", mul(1, 3, 4))
+print("Div: ", div(1, 3, 4))
+
+print("Div0: ", div(1, 1, 4, 0))
