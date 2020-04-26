bc7e8b70fa3c4ac68c3a22d73e2946d36a7f52b5
Peter Pliuta <peter@splynx.com.ua>
2020-04-26 22:02:31 +0300

    hw 5

--- lesson05/classwork_2/classwork.py
+++ lesson05/classwork_2/classwork.py
@@ -1,10 +1,27 @@
 from lib.person import Users
 
+# До роботи яку робили в класі доробити:
+# 1. Реєстрацію нового користувача з перевіркою (перевірити чи коистувач вже є в файлі)
+
 user = Users("John", "Snow", "jsnow", "e@ma.il", "pa$$word")
-# user.show_user_info()
-# user.save_user()
+user.register()
 
-# user2 = Users("John", "Doe", "Snow", "mail", 'pass')
-# user2.save_user()
+user = Users("John", "Snow", "jsnow4", "e@ma.il", "pa$$word")
+user.register()
 
-print(user.show_all_users())
+# 2. Логін користувача (логін по username з перевіркою паролю.)
+
+print("User logged in:", Users.login("jsnow", "pass"))
+print("User logged in:", Users.login("jsnow2", "pass"))
+print("User logged in:", Users.login("jsnow", "pa$$word"))
+
+# 3. Видалення користувача (по username)
+
+result = Users.remove("")
+print("User was removed:", result)
+
+result = Users.remove("jsnow3")
+print("User was removed:", result)
+
+result = Users.remove("jsnow")
+print("User was removed:", result)
--- lesson05/classwork_2/lib/person.py
+++ lesson05/classwork_2/lib/person.py
@@ -5,7 +5,6 @@ class Users:
         self.__username = username
         self.__email = email
         self.__password = password
-        # self.
 
     def show_user_info(self):
         print("First name:", self.__first_name,
@@ -17,25 +16,20 @@ class Users:
               )
 
     def save_user(self):
-        # user = []
-        # user.append(self.__first_name)
-        # user.append(self.__last_name)
-        # user.append(self.__username)
-        # user.append(self.__email)
-        # user.append(self.__password)
+        # f = open("db.txt", "a")
+        # f.write(self.__first_name + '#' +
+        #         self.__last_name + '#' +
+        #         self.__username + '#' +
+        #         self.__email + '#' +
+        #         self.__password + '\n'
+        #         )
+        # f.close()
+        user = self.__dict__.values()
+        with open('db.txt', 'a') as db_file:
+            db_file.write("#".join(user)+"\n")
 
-        f = open("db.txt", "a")
-        f.write(self.__first_name + '#' +
-                self.__last_name + '#' +
-                self.__username + '#' +
-                self.__email + '#' +
-                self.__password + '\n'
-                )
-        f.close()
-
-        # with open('db.txt','a') as db_file:
-        #     db_file.write(", ".join(user)+"\n")
-    def show_all_users(self):
+    @staticmethod
+    def get_all_users():
         users = []
         with open('db.txt') as db_file:
             for line in db_file:
@@ -45,8 +39,44 @@ class Users:
                     users.append(user_properties)
         return users
 
-    def login(self):
-        pass
+    def register(self):
+        """1. Реєстрація нового користувача з перевіркою (перевірити чи користувач вже є в файлі)
+        """
+        user = self.__dict__.values()
+        all_users = self.get_all_users()
+        for item in all_users:
+            if item[2] == self.__username:
+                print("Username '" + item[2] + "' is already present")
+                return
+        with open('db.txt', 'a') as db_file:
+            db_file.write("#".join(user)+"\n")
+
+    @classmethod
+    def login(cls, login, password):
+        """ 2. Логін користувача (логін по username з перевіркою паролю.)
+        """
+        all_users = cls.get_all_users()
+        for item in all_users:
+            # if username is the same, return pass == pass
+            if item[2] == login:
+                return item[4] == password
+        # if no such username in file
+        return False
+
+    @classmethod
+    def remove(cls, login):
+        """3. Видалення користувача (по username)
+        """
+        removed = False
+        all_users = cls.get_all_users()
+
+        with open('db.txt', 'w') as db_file:
+            for item in all_users:
+                if item[2] != login:
+                    db_file.write("#".join(item)+"\n")
+                else:
+                    removed = True
+        return removed
 
     def menu(self):
         pass
