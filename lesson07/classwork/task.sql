d771639a6117f92c77ca655c8a27948c4b77389b
Peter Pliuta <peter@splynx.com.ua>
2020-04-29 19:43:36 +0300

    add backquotes

--- lesson07/classwork/task.sql
+++ lesson07/classwork/task.sql
@@ -2,7 +2,7 @@
 SELECT `onum`, `amt`, `odate` FROM `orders`;
 
 -- 2. Составьте запрос, который выберет строки из таблицы клиентов (customers), номер продавца которых равен 1001.
-SELECT * FROM `customers` where snum=1001;
+SELECT * FROM `customers` where `snum`=1001;
  
 -- 3. Выберите имена клиентов города San Jose и их рейтинг из таблицы клиентов (customers).
 SELECT `cname`, `rating` FROM `customers` where `city` = "San Jose"; 
@@ -11,13 +11,13 @@ SELECT `cname`, `rating` FROM `customers` where `city` = "San Jose";
 SELECT DISTINCT `snum` FROM `orders`;
 
 -- 5. Получите все ряды из таблицы заказов (orders), где сумма заказа больше 1000.
-SELECT * FROM `orders` WHERE amt > 1000;
+SELECT * FROM `orders` WHERE `amt` > 1000;
  
 -- 6. Выберите название города и имена продавцов в Лондоне с размером комиссионных выше 0.11 из таблицы продавцов (salers).
-SELECT city, sname FROM `salers` WHERE comm>0.11;
+SELECT `city`, `sname` FROM `salers` WHERE `comm`>0.11;
 
 -- 7. Выберите всех клиентов из таблицы клиентов (customers), у которых рейтинг меньше или равен 100 и они при этом не из Рима. В запросе должен использоваться оператор NOT.
-SELECT * FROM customers WHERE NOT (rating>100 OR city="Rome");
+SELECT * FROM `customers` WHERE NOT (`rating`>100 OR `city`="Rome");
 
 -- 8. Упростите запрос: "SELECT * FROM salers WHERE comm < 0.12 OR comm = 0.12;".
-SELECT * FROM salers WHERE comm <= 0.12;
+SELECT * FROM `salers` WHERE `comm` <= 0.12;
