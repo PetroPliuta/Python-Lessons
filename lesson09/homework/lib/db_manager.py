f424d49270a8d897142dcea72f23ebb96497e74e
Peter Pliuta <peter@splynx.com.ua>
2020-05-06 22:46:13 +0300

    homework 9

---
+++ lesson09/homework/lib/db_manager.py
@@ -0,0 +1,92 @@
+import mysql.connector
+import requests
+from lib.settings import COVID19_URL
+
+
+class Db_manager:
+    def __init__(self, host, user, passwd):
+        self.__db = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=passwd
+        )
+        self.__cursor = self.__db.cursor(dictionary=True)
+        self.__cursor.execute(
+            "CREATE DATABASE if not exists `python_lesson09`;")
+        self.__cursor.execute("use `python_lesson09`")
+        self.__cursor.execute(
+            "CREATE TABLE if NOT EXISTS Global (id int auto_increment primary key, NewConfirmed int, TotalConfirmed int, NewDeaths int, TotalDeaths int, NewRecovered int, TotalRecovered int, Date DATETIME);")
+        self.__cursor.execute(
+            "CREATE TABLE if NOT EXISTS Countries (id int auto_increment primary key, Country varchar(255), CountryCode varchar(12), Slug varchar(255), NewConfirmed int, TotalConfirmed int, NewDeaths int, TotalDeaths int, NewRecovered int, TotalRecovered int, Date DATETIME);")
+
+    def menu(self):
+        exit = False
+        while not exit:
+            choice = int(input(
+                "1. Update data\n2. Search by country name\n3. Search by country code\n0. Exit\n"))
+            if choice == 1:
+                print("Update")
+                self.__update_covid_data()
+            elif choice == 2:
+                print("Search by country name")
+                self.__search_by_country_name()
+            elif choice == 3:
+                print("Search by country code")
+                self.__search_by_country_code()
+            elif choice == 0:
+                print("Exit")
+                exit = True
+            else:
+                print("Wrong choice")
+
+    def __update_covid_data(self):
+        """1. Update covid database"""
+        covid_data = requests.get(COVID19_URL)
+        covid_data = covid_data.json()
+
+        # Global
+        global_ = covid_data['Global']
+        query = """ INSERT INTO Global (NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date)
+        VALUES ('%s', '%s', '%s', '%s', '%s', '%s', STR_TO_DATE("%s",'%%Y-%%m-%%dT%%TZ'))
+        """ % (global_["NewConfirmed"], global_["TotalConfirmed"], global_["NewDeaths"], global_["TotalDeaths"], global_["NewRecovered"], global_["TotalRecovered"], covid_data['Date'])
+        self.__cursor.execute("TRUNCATE TABLE Global;")
+        self.__cursor.execute(query)
+
+        # Countries
+        self.__cursor.execute("TRUNCATE TABLE Countries;")
+        for item in covid_data['Countries']:
+            query = """
+            INSERT INTO Countries (Country, CountryCode, Slug, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", STR_TO_DATE("%s",'%%Y-%%m-%%dT%%TZ')) 
+            """ % (item['Country'], item['CountryCode'], item['Slug'], item['NewConfirmed'], item['TotalConfirmed'], item['NewDeaths'], item['TotalDeaths'], item['NewRecovered'], item['TotalRecovered'], item['Date'])
+            self.__cursor.execute(query)
+        self.__db.commit()
+
+    def __search_by_country_name(self):
+        """2. Показати інформацію по назві країни (Ukraine)"""
+        country = input("Enter country name (Ukraine): ")
+        self.__cursor.execute(
+            "SELECT Country, CountryCode, Slug, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date FROM Countries WHERE Country='"+country+"'")
+        result = self.__cursor.fetchone()
+        if result == None:
+            print("Country '"+country+"' not found")
+            return
+        # print(result)
+        print("-"*10+" Country info "+"-"*10)
+        for item in result:
+            print(item, ": ", result[item], sep="")
+        print("-"*30)
+
+    def __search_by_country_code(self):
+        """3. Показати інформацію по коду країни (UA)"""
+        code = input("Enter country code (UA): ")
+        self.__cursor.execute(
+            "SELECT Country, CountryCode, Slug, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date FROM Countries WHERE CountryCode='"+code+"'")
+        result = self.__cursor.fetchone()
+        if result == None:
+            print("Country with code '"+code+"' not found")
+            return
+        # print(result)
+        print("-"*10+" Country info "+"-"*10)
+        for item in result:
+            print(item, ": ", result[item], sep="")
+        print("-"*33)
---
+++ lesson09/homework/lib/settings.py
@@ -0,0 +1,4 @@
+HOST = "127.0.0.1"
+USERNAME = "root"
+PASSWORD = ""
+COVID19_URL = 'https://api.covid19api.com/summary'
